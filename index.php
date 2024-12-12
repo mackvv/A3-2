6ac2c076800c48ac3a9090045ba85f8ca9c02f1c
Mackenzie Van Vliet <mackenzievanvliet@gmail.com>
2024-12-12 02:59:05 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+echo '<img src="https://img.freepik.com/premium-photo/cat-wearing-sunglasses-with-sun-shining-it_910054-8427.jpg?w=360" alt="Cat wearing sunglasses" style="max-width: 100%; height: auto;">';
+?>
